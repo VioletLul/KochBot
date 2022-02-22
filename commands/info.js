dfaaf8adac1e316d80cde152f3533b0278ae4dc1
Violet#0461 <J.roesser-ecdl@t-online.de>
2022-02-22 10:08:19 +0100

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -12,7 +12,7 @@ module.exports = {
         {name: 'Prefix', value: 'Dies ist der Prefix: F.'},
         {name: 'Coder', value: '^^Violet#0461'},
         {name: 'Für wen ist dieser Bot?', value: "Dieser Bot ist für Fouldeer und seinen Koch Discord."},
-        {name: 'Commands', value: "F.info (Dieser Embed wird angezeigt) \nF.spaghetti (Rezept für Spaghetti wird angezeigt!)\nF.obatzten (Selbst erklärend was dann passiert :))\nF.play {liedname/youtube URL} => Lässt den Bot in einen Channel joinen und spielt das Lied ab.\nF.leave => Lässt den Bot aus dem Channel leaven."}
+        {name: 'Commands', value: "F.info (Dieser Embed wird angezeigt)\nF.spaghetti (Rezept für Spaghetti wird angezeigt!)\nF.obatzten (Selbst erklärend was dann passiert :))\nF.play {liedname/youtube URL} => Lässt den Bot in einen Channel joinen und spielt das Lied ab.\nF.leave => Lässt den Bot aus dem Channel leaven."}
       )
       message.channel.send(newEmbed)
 
