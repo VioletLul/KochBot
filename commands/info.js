0e264cd2658d777c626b8fe3c1b86b7bb161e0fe
Violet#0461 <J.roesser-ecdl@t-online.de>
2022-02-24 11:00:42 +0100

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -3,7 +3,7 @@ module.exports = {
     description: "Info zu mir, dem Kochbot!",
     execute(message, args, Discord){
 
-      const newEmbed = new Discord.MessageEmbed()
+      const infoembed = new Discord.MessageEmbed()
       .setColor('YELLOW')
       .setTitle('Infos')
       .setThumbnail("https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Messagebox_info.svg/800px-Messagebox_info.svg.png")
@@ -14,7 +14,7 @@ module.exports = {
         {name: 'Für wen ist dieser Bot?', value: "Dieser Bot ist für Fouldeer und seinen Koch Discord."},
         {name: 'Commands', value: "F.info (Dieser Embed wird angezeigt)\nF.spaghetti (Rezept für Spaghetti wird angezeigt!)\nF.obatzten (Selbst erklärend was dann passiert :))\nF.play {liedname/youtube URL} => Lässt den Bot in einen Channel joinen und spielt das Lied ab.\nF.leave => Lässt den Bot aus dem Channel leaven."}
       )
-      message.channel.send(newEmbed)
+      message.channel.send(infoembed)
 
       }
 }
