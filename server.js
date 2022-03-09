3393c16c6b420535931861bf9e1c3b8a0389c06b
Violet#0461 <J.roesser-ecdl@t-online.de>
2022-03-09 13:30:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
+//Importing the package "express"
 const express = require("express")
-
+//creating new variable
 const server = express()
 
+//creates a cloud server wich goes offline after 15 minutes inactivity
 server.all("/", (req, res) => {
     res.send(`Moin! Dieser Server ist exlusiv für den KochBot!`)
 })
 
+/**
+ * Opens a port at port 3000 and logs the text in the console
+ */
 function keepalive() {
     server.listen(3000, () => {
         console.log("Server ist gestartet. Warum bin ich hier?")
     })
 }
 
+//makes keepalive ti a module and exports it
 module.exports = keepalive
